0b7259529a7fee40b1e4c62dd9b42119fc964921
Karlo Calzada <karlo.calzada2798@gmail.com>
2021-04-14 19:53:02 +0800

    script from index.html is transfered in redirect.js and doctor name now displays name on modal.

--- Scripts/redirect.js
+++ Scripts/redirect.js
@@ -1,3 +1,8 @@
-function MainPageRedirectFunc() {
-  location.replace("../index.html")
-}
+$(document).on('click','table tbody tr',function(){
+  $("#schedule-modal").modal("show");
+  var text = $(this).text().split("\n");
+
+  var name = text[1] + text[2];
+  console.log(name);
+  $(".modal-title").html(name);
+})
